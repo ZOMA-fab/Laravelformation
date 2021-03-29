3838dbcaa1f96e40f57171b3d2fc80f4e7108473
Your Name <you@example.com>
2021-03-29 17:07:31 +0000

    Premier commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ Route::get('modification/{id}',      [MainController::class, 'modifierProduit'])
 Route::get('supprimer/{id}',         [MainController::class, 'supprimerProduit'])->name('supprimer.produit') ;
 Route::get('ajoutercommande/{id}',   [MainController::class, 'ajoutercommande'])->name('ajoutercommande') ;
 Route::get('produits/liste',         [MainController::class, 'getList'])->name('produits.liste') ;
+Route::post("produits/enregistrer",  [MainController::class, "enregistrerProduit"])->name('produits.enregistrer');
+//Route::get("produits/modifier/{id}", [MainController::class, "editproduit"])->name('produits.modifier');
+Route::get("produits/modifier/{produit}", [ProduitController::class, "edit"])->name('produits.modifier');
+Route::put("produits/update/{id}",   [MainController::class, "updateProduit"])->name('produits.update');
+Route::resource("produits",          ProduitController::class);
+Route::get("export-excel",           [MainController::class, "excelExport"])->name('excel.export');
